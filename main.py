5deb3e6d68f418475ec5caad914511d60d2fba29
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2025-09-16 10:42:36 +0300

    Добавил новую функцию прощания

--- main.py
+++ main.py
@@ -15,8 +15,13 @@ def delit_nums(n1, n2):
     print(f'{n1}/{n2}={n1/n2}')
 
 
+def bye_bye():
+    print('пока, пользователь')
+
+
 if __name__ == '__main__':
     hi_func()
     n1, n2 = input_nums()
     minus_nums(n1, n2)
     delit_nums(n1, n2)
+    bye_bye()
