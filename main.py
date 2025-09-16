0e10718ea334c1915afac5a8493e93eab2ed256a
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2025-09-16 08:58:13 +0300

    Добавил ввод двух целых чисел

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ def hi_func():
     print('Hello')
 
 
+def input_nums():
+    n1, n2 = int(input('Ввод первого числа: ')), int(input('Ввод второго числа: '))
+
+
 if __name__ == '__main__':
     hi_func()
+    input_nums()
