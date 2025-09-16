759d70d6c12048d06437e2d4dd69a8eae836a792
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2025-09-16 09:22:55 +0300

    Добавил вывод операции сложения с числами

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ def hi_func():
 
 def input_nums():
     n1, n2 = int(input('Ввод первого числа: ')), int(input('Ввод второго числа: '))
+    return n1, n2
+
+
+def plus_nums(n1, n2):
+    print(f'{n1}+{n2}={n1+n2}')
 
 
 if __name__ == '__main__':
     hi_func()
-    input_nums()
+    n1, n2 = input_nums()
+    plus_nums(n1, n2)
